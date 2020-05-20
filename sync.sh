d3b2338ab0f3bf226ae6175ce20bae6e8d69e86d
bigbule33 <csalwan@us.ibm.com>
2020-05-20 10:24:29 -0400

    commit

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "$now"
+echo "Current date: $now"
 echo "git add --all"
 git add --all
-echo "git commit -m $date"
-git commit -m "commit $date"
+echo "git commit -m $now"
+git commit -m "commit $now"
 echo "git push -u origin master"
 git push -u origin master
 echo done
